df0a15456cc0076ac58ddda5d6a042eda319386d
starlingvibes <starlingroot@protonmail.com>
2023-05-20 23:47:27 +0100

    feat(): initial project setup

---
+++ python/src/auth/server.py
@@ -0,0 +1,16 @@
+import jwt
+import datetime
+import os
+from flask import Flask
+import request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# configurations
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
